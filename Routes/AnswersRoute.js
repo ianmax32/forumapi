25f359321270c072a86e881c17682ac08321fb25
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-03-08 09:07:38 +0200

    refactor: update answers patch request

--- Routes/AnswersRoute.js
+++ Routes/AnswersRoute.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const router = express.Router()
-const Question = require('../Models/Question');
+const express = require("express");
+const router = express.Router();
+const Question = require("../Models/Question");
 
-router.get('/:id', async (req,res)=>{
-    try {
-        const question = await  Question.find({_id:req.params.id});
-        res.json({question});
-    } catch (error) {
-        res.json({message:err});
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const question = await Question.find({ _id: req.params.id });
+    res.json({ question });
+  } catch (error) {
+    res.json({ message: err });
+  }
 });
 
-router.patch('/:id', async (req,res)=>{
-    try {
-        const question = await  Question.updateOne({_id:req.params.id},{$set:{}});
-        res.json({question});
-    } catch (error) {
-        res.json({message:err});
-    }
+router.patch("/:id", async (req, res) => {
+  try {
+    const answer = req.body.answer;
+    const question = await Question.updateOne(
+      { _id: req.params.id },
+      { $push: { answers: answer } }
+    );
+    res.json(question);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
 module.exports = router;
